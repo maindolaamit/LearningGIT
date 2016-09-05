73dc1e916dbaa766ae7ea15bbb487534b5b1db0f
maindolaamit <maindola.amit@gmail.com>
2016-09-05 18:25:49 +0530

    Added game Controller

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -1,1 +1,26 @@
 var app = angular.module("game-app", []);
+
+app.controller("game-controller", function ($scope) {
+    var createCell = function (){
+        return {
+            value : null,
+            editable : function () {
+                return !value;
+            }
+        };
+    };
+    var game = {
+        mode: {
+            running: true,
+            xPlayerTurn: false
+        },
+        board:{
+            cells: [
+                createCell(), createCell(), createCell(),
+                createCell(), createCell(), createCell(),
+                createCell(), createCell(), createCell()]
+        }
+    };
+
+    $scope.game = game;
+});
