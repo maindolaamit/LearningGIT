7a0c91eda491ecf14173ff08afc139faededa8b9
maindolaamit <maindola.amit@gmail.com>
2016-09-09 18:51:18 +0530

    added result

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -1,28 +1,45 @@
 var app = angular.module("game-app", []);
 
 app.controller("game-controller", function ($scope) {
-    var createCell = function (){
+    // Function to create a new cell
+    var createCell = function () {
         return {
-            value : null,
-            editable : function () {
+            value: null,
+            editable: function () {
                 return !value;
             }
         };
     };
+    // Function to control game
     var game = {
         mode: {
-            running: true,
-            xPlayerTurn: true
+            running: true, xPlayerTurn: true, filledCells: 0
         },
-        board:{
-            select: function(cell){
-                cell.value = game.mode.xPlayerTurn ? 'X' : 'O';
-                game.mode.xPlayerTurn = !game.mode.xPlayerTurn;
-            },
+        board: {
+            // create cells in board container
             cells: [
                 createCell(), createCell(), createCell(),
                 createCell(), createCell(), createCell(),
-                createCell(), createCell(), createCell()]
+                createCell(), createCell(), createCell()],
+            // function when cell is clicked
+            select: function (cell) {
+                cell.value = game.mode.xPlayerTurn ? 'X' : 'O';
+                game.mode.xPlayerTurn = !game.mode.xPlayerTurn;
+                game.filledCells = game.filledCells + 1; //increase value by 1
+
+                if (game.filledCells = 9) {
+                } else {
+                    // Call result
+                    gameResult();
+                }
+            }
+        }
+    };
+    // Function to check result
+    var gameResult = function () {
+        // Function to check values of cell array to determine result
+        result: {
+            message: "Player 1 wins !!!"
         }
     };
 
