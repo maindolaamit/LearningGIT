a8ed59d4b3900f008e25a7aa9990361014c4eb9e
maindolaamit <maindola.amit@gmail.com>
2016-09-09 20:42:05 +0530

    added game result.

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -13,7 +13,8 @@ app.controller("game-controller", function ($scope) {
     // Function to control game
     var game = {
         mode: {
-            running: true, xPlayerTurn: true, filledCells: 0
+            running: true, xPlayerTurn: true,
+            filledCells: 0, gameResult: null
         },
         board: {
             // create cells in board container
@@ -25,23 +26,52 @@ app.controller("game-controller", function ($scope) {
             select: function (cell) {
                 cell.value = game.mode.xPlayerTurn ? 'X' : 'O';
                 game.mode.xPlayerTurn = !game.mode.xPlayerTurn;
-                game.filledCells = game.filledCells + 1; //increase value by 1
+                game.mode.filledCells = game.mode.filledCells + 1; //increase value by 1
 
-                if (game.filledCells = 9) {
-                } else {
-                    // Call result
-                    gameResult();
-                }
+                // check Result
+                checkResult();
             }
         }
     };
+    // Function to return cell value
+    var cellValue = function (position) {
+        return game.board.cells[position].value;
+    };
     // Function to check result
-    var gameResult = function () {
-        // Function to check values of cell array to determine result
-        result: {
-            message: "Player 1 wins !!!"
+    var checkResult = function () {
+        if (cellValue(1) == cellValue(2) && cellValue(3) == cellValue(2)) {
+            game.mode.gameResult = resultMessage(cellValue(1));
+        } else if (cellValue(4) == cellValue(5) && cellValue(6) == cellValue(5)) {
+            game.mode.gameResult = resultMessage(cellValue(4));
+        } else if (cellValue(7) == cellValue(8) && cellValue(1) == cellValue(8)) {
+            game.mode.gameResult = resultMessage(cellValue(7));
+        } else if (cellValue() == cellValue(4) && cellValue(7) == cellValue(4)) {
+            game.mode.gameResult = resultMessage(cellValue(1));
+        } else if (cellValue(2) == cellValue(5) && cellValue(8) == cellValue(8)) {
+            game.mode.gameResult = resultMessage(cellValue(2));
+        } else if (cellValue(3) == cellValue(6) && cellValue(1) == cellValue(6)) {
+            game.mode.gameResult = resultMessage(cellValue(3));
+        } else if (cellValue(1) == cellValue(5) && cellValue(5) == cellValue(1)) {
+            game.mode.gameResult = resultMessage(cellValue(1));
+        } else if (cellValue(3) == cellValue(5) && cellValue(5) == cellValue(7)) {
+            game.mode.gameResult = resultMessage(cellValue(3));
+        } else {
+            // When no match and all cells are filled game is draw
+            if (game.mode.filledCells == 9) {
+                // game.mode.gameResult = "Filled cells " + game.mode.filledCells;
+                game.mode.gameResult = "Game is a Draw !!!";
+            }
+        }
+    };
+
+    var resultMessage = function (value) {
+        if (value == "X") {
+            return "Player 1 Wins !!!";
+        } else {
+            return "Player 2 Wins !!!";
         }
     };
 
     $scope.game = game;
-});
+})
+;
